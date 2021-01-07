3ef384e2a37a39b46f0235cc85ab00bd20e19ce0
BrandyBecker <brandybeckerfsw@gmail.com>
2021-01-07 13:51:35 -0500

    adds links to specific nav buttons

---
+++ portfolio/src/components/Blog/blog.md
@@ -0,0 +1,13 @@
+# Day 1
+Starting as a Part Time Labs TL
+Starting on Portfolio
+Eager to start coding atleast an hour a day
+Eager to do atleast 3 code challenges a day
+Eager to work on atleast 1 coding project a day
+
+Checklist:
+[x] started part time tl
+[x] starting on portfolio
+[x] coded at least 1hr
+[x] started on portfolio project
+[] completed at least 3 code challenges
--- portfolio/src/components/Nav/Nav.js
+++ portfolio/src/components/Nav/Nav.js
@@ -4,19 +4,31 @@ function Nav() {
   return (
     <div className="Nav">
         <div class="nav-header">
-            <a class="contact" href="#" target="_blank">contact</a>
+            <a class="contact" href="mailto:Brandy<brandybeckerfsw@gmail.com">
+                <i class="far fa-envelope"></i>
+            </a>
+            
             <h3>brandy becker : software engineer</h3>
+
             <div class="nav-socials">
-                <a class="fb" href="#" target="_blank">facebook</a>
-                <a class="gh" href="#" target="_blank">github</a>
-                <a class="li" href="#" target="_blank">linked in</a>
+                <a class="fb" href="https://www.facebook.com/16brbecker/" target="_blank">
+                    <i class="fab fa-facebook-square"></i>
+                </a>
+                <a class="gh" href="https://github.com/brandybecker/" target="_blank">
+                    <i class="fab fa-github-square"></i>
+                </a>
+                <a class="li" href="https://www.linkedin.com/in/brandybeckerfsw/" target="_blank">
+                    <i class="fab fa-linkedin"></i>
+                </a>
             </div>
         </div>
+
         <div class="nav-footer">
             <a class="projects" href="#" target="_blank">projects</a>
             <a class="blog" href="#" target="_blank">blog</a>
             <a class="about" href="#" target="_blank">about</a>
         </div>
+        
     </div>
   );
 }
